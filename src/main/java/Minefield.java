4d9e7007593e07bb3af64fad0f80d9d1eb568c11
SeungYongShim <syshim@me.com>
2022-05-08 14:18:29 +0900

    .

--- src/main/java/Minefield.java
+++ src/main/java/Minefield.java
@@ -40,26 +40,26 @@ public class Minefield {
     public Stream<MineItem> nearItems(MineItem item)
     {
         // 튜플이 없어!!!!
-        var builderPosNear = Stream.<Tuple>builder();
-        builderPosNear.add(new Tuple(item.getX() - 1, item.getY() - 1));
-        builderPosNear.add(new Tuple(item.getX() , item.getY() - 1));
-        builderPosNear.add(new Tuple(item.getX() + 1, item.getY() - 1));
-        builderPosNear.add(new Tuple(item.getX() - 1, item.getY() ));
-        builderPosNear.add(new Tuple(item.getX() + 1, item.getY() ));
-        builderPosNear.add(new Tuple(item.getX() - 1, item.getY() + 1));
-        builderPosNear.add(new Tuple(item.getX() , item.getY() + 1));
-        builderPosNear.add(new Tuple(item.getX() + 1, item.getY() + 1));
+        var builder = Stream.<Tuple>builder();
+        builder.add(new Tuple(item.getX() - 1, item.getY() - 1));
+        builder.add(new Tuple(item.getX() , item.getY() - 1));
+        builder.add(new Tuple(item.getX() + 1, item.getY() - 1));
+        builder.add(new Tuple(item.getX() - 1, item.getY() ));
+        builder.add(new Tuple(item.getX() + 1, item.getY() ));
+        builder.add(new Tuple(item.getX() - 1, item.getY() + 1));
+        builder.add(new Tuple(item.getX() , item.getY() + 1));
+        builder.add(new Tuple(item.getX() + 1, item.getY() + 1));
         
-        return builderPosNear.build()
-                             .map(i -> 
-                             {
-                                 if(i.x() < 0) return null;
-                                 if(i.x() >= this.width) return null;
-                                 if(i.y() < 0) return null;
-                                 if(i.y() >= this.height) return null;
-                                 return mineItems.get(i.x() + i.y() * width);
-                             })
-                             .filter(x -> x != null);
+        return builder.build()
+                      .map(i -> 
+                      {
+                          if(i.x() < 0) return null;
+                          if(i.x() >= this.width) return null;
+                          if(i.y() < 0) return null;
+                          if(i.y() >= this.height) return null;
+                          return mineItems.get(i.x() + i.y() * width);
+                      })
+                      .filter(x -> x != null);
     }
 }
 
